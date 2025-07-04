fdabc3f7b6a76da6df812b5b39b460690694404e
shujie_huang <sergio.shujie.huang@gmail.com>
2025-07-04 16:51:38 +0900

    Add conda and pip environment files; modify test.py script

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("test")
+import gymnasium as gym
+
+env = gym.make('CartPole-v1', render_mode='human')
+env.reset()
+for _ in range(100):
+    env.render()
+    env.step(env.action_space.sample())
